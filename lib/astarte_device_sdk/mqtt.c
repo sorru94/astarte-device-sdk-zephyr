575de03ec6b09f7778df59317b5f60d0857c5081
Simone Orru <simone.orru@secomind.com>
2024-04-29 09:57:42 +0200

    Split MQTT handle events callback
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- lib/astarte_device_sdk/mqtt.c
+++ lib/astarte_device_sdk/mqtt.c
@@ -45,14 +45,60 @@ static sec_tag_t sec_tag_list[] = {
  ***********************************************/
 
 /**
- * @brief Helper function to parse an incoming published message.
+ * @brief Handle a CONNACK reception event.
  *
  * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
- * @param[in] pub Received published data in the MQTT client format.
- * @return The number of bytes received upon success, a negative error (errno.h) otherwise.
+ * @param[in] connack Received CONNACK data in the MQTT client format.
  */
-static ssize_t handle_published_message(
-    astarte_mqtt_t *astarte_mqtt, const struct mqtt_publish_param *pub);
+static void handle_connack_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_connack_param connack);
+/**
+ * @brief Handle a disconnection event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ */
+static void handle_disconnection_event(astarte_mqtt_t *astarte_mqtt);
+/**
+ * @brief Handle a PUBLISH reception event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ * @param[in] publish Received PUBLISH data in the MQTT client format.
+ */
+static void handle_publish_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_publish_param publish);
+/**
+ * @brief Handle a PUBREL reception event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ * @param[in] pubrel Received PUBREL data in the MQTT client format.
+ */
+static void handle_pubrel_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_pubrel_param pubrel);
+/**
+ * @brief Handle a PUBACK reception event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ * @param[in] puback Received PUBACK data in the MQTT client format.
+ */
+static void handle_puback_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_puback_param puback);
+/**
+ * @brief Handle a PUBREC reception event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ * @param[in] pubrec Received PUBREC data in the MQTT client format.
+ */
+static void handle_pubrec_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_pubrec_param pubrec);
+/**
+ * @brief Handle a PUBCOMP reception event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ * @param[in] pubcomp Received PUBCOMP data in the MQTT client format.
+ */
+static void handle_pubcomp_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_pubcomp_param pubcomp);
+/**
+ * @brief Handle a SUBACK reception event.
+ *
+ * @param[in] astarte_mqtt Handle to the Astarte MQTT client instance.
+ * @param[in] suback Received SUBACK data in the MQTT client format.
+ */
+static void handle_suback_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_suback_param suback);
 
 /************************************************
  *       Callbacks declaration/definition       *
@@ -60,98 +106,71 @@ static ssize_t handle_published_message(
 
 static void mqtt_evt_handler(struct mqtt_client *const client, const struct mqtt_evt *evt)
 {
-    int res = 0;
     astarte_mqtt_t *astarte_mqtt = CONTAINER_OF(client, astarte_mqtt_t, client);
 
     switch (evt->type) {
         case MQTT_EVT_CONNACK:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT connect failed %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT CONNACK event error: %s", strerror(-evt->result));
                 break;
             }
-            ASTARTE_LOG_DBG("MQTT client connected");
-            astarte_mqtt->is_connected = true;
-            on_connected(astarte_mqtt, evt->param.connack);
+            handle_connack_event(astarte_mqtt, evt->param.connack);
             break;
-
         case MQTT_EVT_DISCONNECT:
-            ASTARTE_LOG_DBG("MQTT client disconnected %d", evt->result);
-            astarte_mqtt->is_connected = false;
-            on_disconnected(astarte_mqtt);
+            if (evt->result != 0) {
+                ASTARTE_LOG_ERR("MQTT disconnection event error: %s", strerror(-evt->result));
+            }
+            handle_disconnection_event(astarte_mqtt);
             break;
-
         case MQTT_EVT_PUBLISH:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT publish reception failed %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT PUBLISH event error: %s", strerror(-evt->result));
                 break;
             }
-
-            const struct mqtt_publish_param *pub = &evt->param.publish;
-            res = handle_published_message(astarte_mqtt, pub);
-            if ((res < 0) || (res != pub->message.payload.len)) {
-                ASTARTE_LOG_ERR("MQTT published incoming data parsing error %d", res);
-                break;
-            }
-
+            handle_publish_event(astarte_mqtt, evt->param.publish);
             break;
-
         case MQTT_EVT_PUBREL:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT PUBREL error %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT PUBREL event error: %s", strerror(-evt->result));
                 break;
             }
-            ASTARTE_LOG_DBG("PUBREL packet id: %u", evt->param.pubrel.message_id);
-
-            struct mqtt_pubcomp_param pubcomp = { .message_id = evt->param.pubrel.message_id };
-            res = mqtt_publish_qos2_complete(client, &pubcomp);
-            if (res != 0) {
-                ASTARTE_LOG_ERR("MQTT PUBCOMP transmission error %d", res);
-            }
-
+            handle_pubrel_event(astarte_mqtt, evt->param.pubrel);
             break;
-
         case MQTT_EVT_PUBACK:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT PUBACK error %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT PUBACK event error: %s", strerror(-evt->result));
                 break;
             }
-            ASTARTE_LOG_DBG("PUBACK packet id: %u", evt->param.puback.message_id);
+            handle_puback_event(astarte_mqtt, evt->param.puback);
             break;
-
         case MQTT_EVT_PUBREC:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT PUBREC error %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT PUBREC event error: %s", strerror(-evt->result));
                 break;
             }
-            ASTARTE_LOG_DBG("PUBREC packet id: %u", evt->param.pubrec.message_id);
-            const struct mqtt_pubrel_param rel_param
-                = { .message_id = evt->param.pubrec.message_id };
-            int err = mqtt_publish_qos2_release(client, &rel_param);
-            if (err != 0) {
-                ASTARTE_LOG_ERR("Failed to send MQTT PUBREL: %d", err);
-            }
+            handle_pubrec_event(astarte_mqtt, evt->param.pubrec);
             break;
-
         case MQTT_EVT_PUBCOMP:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT PUBCOMP error %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT PUBCOMP event error: %s", strerror(-evt->result));
                 break;
             }
-            ASTARTE_LOG_DBG("PUBCOMP packet id: %u", evt->param.pubcomp.message_id);
+            handle_pubcomp_event(astarte_mqtt, evt->param.pubcomp);
             break;
-
         case MQTT_EVT_SUBACK:
             if (evt->result != 0) {
-                ASTARTE_LOG_ERR("MQTT SUBACK error %d", evt->result);
+                ASTARTE_LOG_ERR("MQTT SUBACK event error: %s", strerror(-evt->result));
                 break;
             }
-            ASTARTE_LOG_DBG("SUBACK packet id: %u", evt->param.suback.message_id);
+            handle_suback_event(astarte_mqtt, evt->param.suback);
             break;
-
         case MQTT_EVT_PINGRESP:
-            ASTARTE_LOG_DBG("PINGRESP packet");
+            if (evt->result != 0) {
+                ASTARTE_LOG_ERR("MQTT PINGRESP event error: %s", strerror(-evt->result));
+                break;
+            }
+            ASTARTE_LOG_DBG("Received PINGRESP packet");
             break;
-
         default:
             ASTARTE_LOG_WRN("Unhandled MQTT event: %d", evt->type);
             break;
@@ -247,6 +266,8 @@ astarte_result_t astarte_mqtt_disconnect(astarte_mqtt_t *astarte_mqtt)
 
 astarte_result_t astarte_mqtt_subscribe(astarte_mqtt_t *astarte_mqtt, const char *topic)
 {
+    ASTARTE_LOG_DBG("Subscribing to %s", topic);
+
     struct mqtt_topic topics[] = { {
         .topic = { .utf8 = topic, .size = strlen(topic) },
         .qos = 2,
@@ -256,8 +277,6 @@ astarte_result_t astarte_mqtt_subscribe(astarte_mqtt_t *astarte_mqtt, const char
         .list_count = ARRAY_SIZE(topics),
         .message_id = astarte_mqtt->message_id++,
     };
-
-    ASTARTE_LOG_DBG("Subscribing to %s", topic);
 
     int ret = mqtt_subscribe(&astarte_mqtt->client, &ctrl_sub_list);
     if (ret != 0) {
@@ -328,59 +347,124 @@ astarte_result_t astarte_mqtt_poll(astarte_mqtt_t *astarte_mqtt)
  *         Static functions definitions         *
  ***********************************************/
 
-static ssize_t handle_published_message(
-    astarte_mqtt_t *astarte_mqtt, const struct mqtt_publish_param *pub)
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_connack_event(
+    astarte_mqtt_t *astarte_mqtt, const struct mqtt_connack_param connack)
 {
+    ASTARTE_LOG_DBG("Received CONNACK packet");
+    astarte_mqtt->is_connected = true;
+    on_connected(astarte_mqtt, connack);
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_disconnection_event(astarte_mqtt_t *astarte_mqtt)
+{
+    ASTARTE_LOG_DBG("MQTT client disconnected");
+    astarte_mqtt->is_connected = false;
+    on_disconnected(astarte_mqtt);
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_publish_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_publish_param publish)
+{
+    uint16_t message_id = publish.message_id;
+    ASTARTE_LOG_DBG("Received PUBLISH packet (%u)", message_id);
     int ret = 0U;
     size_t received = 0U;
-    uint32_t message_size = pub->message.payload.len;
+    uint32_t message_size = publish.message.payload.len;
     char msg_buffer[MQTT_MAX_MSG_SIZE] = { 0 };
     const bool discarded = message_size > MQTT_MAX_MSG_SIZE;
 
     ASTARTE_LOG_DBG("RECEIVED on topic \"%.*s\" [ id: %u qos: %u ] payload: %u / %u B",
-        pub->message.topic.topic.size, (const char *) pub->message.topic.topic.utf8,
-        pub->message_id, pub->message.topic.qos, message_size, MQTT_MAX_MSG_SIZE);
+        publish.message.topic.topic.size, (const char *) publish.message.topic.topic.utf8,
+        message_id, publish.message.topic.qos, message_size, MQTT_MAX_MSG_SIZE);
 
     while (received < message_size) {
         char *pkt = discarded ? msg_buffer : &msg_buffer[received];
 
         ret = mqtt_read_publish_payload_blocking(&astarte_mqtt->client, pkt, MQTT_MAX_MSG_SIZE);
         if (ret < 0) {
-            return ret;
+            ASTARTE_LOG_ERR("Error reading payload of PUBLISH packet %s", strerror(ret));
+            return;
         }
 
         received += ret;
     }
 
-    if (pub->message.topic.qos == MQTT_QOS_1_AT_LEAST_ONCE) {
-        struct mqtt_puback_param puback = { .message_id = pub->message_id };
+    if (publish.message.topic.qos == MQTT_QOS_1_AT_LEAST_ONCE) {
+        struct mqtt_puback_param puback = { .message_id = message_id };
         ret = mqtt_publish_qos1_ack(&astarte_mqtt->client, &puback);
         if (ret != 0) {
             ASTARTE_LOG_ERR("MQTT PUBACK transmission error %d", ret);
         }
     }
-    if (pub->message.topic.qos == MQTT_QOS_2_EXACTLY_ONCE) {
-        struct mqtt_pubrec_param pubrec = { .message_id = pub->message_id };
+    if (publish.message.topic.qos == MQTT_QOS_2_EXACTLY_ONCE) {
+        struct mqtt_pubrec_param pubrec = { .message_id = message_id };
         ret = mqtt_publish_qos2_receive(&astarte_mqtt->client, &pubrec);
         if (ret != 0) {
             ASTARTE_LOG_ERR("MQTT PUBREC transmission error %d", ret);
         }
     }
 
-    if (!discarded) {
-        ASTARTE_LOG_HEXDUMP_DBG(msg_buffer, MIN(message_size, 256U), "Received payload:");
-
-        // This copy is necessary due to the Zephyr MQTT library not null terminating the topic.
-        size_t topic_len = pub->message.topic.topic.size;
-        char *topic = calloc(topic_len + sizeof(char), sizeof(char));
-        if (!topic) {
-            ASTARTE_LOG_ERR("Out of memory %s: %d", __FILE__, __LINE__);
-            return -ENOMEM;
-        }
-        memcpy(topic, pub->message.topic.topic.utf8, topic_len);
-        on_incoming(astarte_mqtt, topic, topic_len, msg_buffer, message_size);
-        free(topic);
+    if (received != publish.message.payload.len) {
+        ASTARTE_LOG_ERR("Incoherent expected and read MQTT publish payload lengths.");
+        return;
     }
 
-    return discarded ? -ENOMEM : (ssize_t) received;
+    if (discarded) {
+        ASTARTE_LOG_ERR("Insufficient space in the Astarte MQTT reception buffer.");
+        ASTARTE_LOG_ERR("Out of memory %s: %d", __FILE__, __LINE__);
+        return;
+    }
+
+    ASTARTE_LOG_HEXDUMP_DBG(msg_buffer, MIN(message_size, 256U), "Received payload:");
+
+    // This copy is necessary due to the Zephyr MQTT library not null terminating the topic.
+    size_t topic_len = publish.message.topic.topic.size;
+    char *topic = calloc(topic_len + sizeof(char), sizeof(char));
+    if (!topic) {
+        ASTARTE_LOG_ERR("Out of memory %s: %d", __FILE__, __LINE__);
+        return;
+    }
+    memcpy(topic, publish.message.topic.topic.utf8, topic_len);
+    on_incoming(astarte_mqtt, topic, topic_len, msg_buffer, message_size);
+    free(topic);
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_pubrel_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_pubrel_param pubrel)
+{
+    uint16_t message_id = pubrel.message_id;
+    ASTARTE_LOG_DBG("Received PUBREL packet (%u)", message_id);
+
+    struct mqtt_pubcomp_param pubcomp = { .message_id = message_id };
+    int res = mqtt_publish_qos2_complete(&astarte_mqtt->client, &pubcomp);
+    if (res != 0) {
+        ASTARTE_LOG_ERR("MQTT PUBCOMP transmission error %d", res);
+    }
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_puback_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_puback_param puback)
+{
+    ASTARTE_LOG_DBG("Received PUBACK packet (%u)", puback.message_id);
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_pubrec_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_pubrec_param pubrec)
+{
+    uint16_t message_id = pubrec.message_id;
+    ASTARTE_LOG_DBG("Received PUBREC packet (%u)", message_id);
+
+    // Transmit a PUBREL
+    const struct mqtt_pubrel_param rel_param = { .message_id = message_id };
+    int err = mqtt_publish_qos2_release(&astarte_mqtt->client, &rel_param);
+    if (err != 0) {
+        ASTARTE_LOG_ERR("Failed to send MQTT PUBREL: %d", err);
+    }
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_pubcomp_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_pubcomp_param pubcomp)
+{
+    ASTARTE_LOG_DBG("Received PUBCOMP packet (%u)", pubcomp.message_id);
+}
+// NOLINTNEXTLINE(misc-unused-parameters)
+static void handle_suback_event(astarte_mqtt_t *astarte_mqtt, struct mqtt_suback_param suback)
+{
+    ASTARTE_LOG_DBG("Received SUBACK packet (%u)", suback.message_id);
 }

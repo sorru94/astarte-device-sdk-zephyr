68d5ebdfe630179ad6390c2c5dac6547c4b01353
Luca Arato <luca.arato@secomind.com>
2024-06-11 10:02:49 +0200

    Prepare release 0.6.0

--- include/astarte_device_sdk/astarte.h
+++ include/astarte_device_sdk/astarte.h
@@ -19,7 +19,7 @@
 /** @brief Major version number */
 #define ASTARTE_DEVICE_SDK_MAJOR 0
 /** @brief Minor version number */
-#define ASTARTE_DEVICE_SDK_MINOR 5
+#define ASTARTE_DEVICE_SDK_MINOR 6
 /** @brief Patch version number */
 #define ASTARTE_DEVICE_SDK_PATCH 0
 

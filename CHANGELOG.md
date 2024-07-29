8927e39ba080556883afd5414bce02d5ca72eb2d
Simone Orru <simone.orru@secomind.com>
2024-07-29 12:28:47 +0200

    Compatibility with Zephyr 3.7.0
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.1.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [0.7.0] - Unreleased
+### Added
+- Support for Zephyr 3.7.
+
+### Removed
+- Support for Zephyr 3.6 in the code samples and tests.
+
 ## [0.6.1] - 2024-06-28
 ### Fixed
 - Device state is changed to CONNECTED or DISCONNECTED before calling the appropriate callback.
--- tests/lib/astarte_device_sdk/integration/include/native-sim-tls-conf.h
+++ tests/lib/astarte_device_sdk/integration/include/native-sim-tls-conf.h
@@ -7,32 +7,20 @@
 #ifndef NATIVE_SIM_TLS_CONF_H
 #define NATIVE_SIM_TLS_CONF_H
 
-#define MBEDTLS_BIGNUM_C
-
-#define MBEDTLS_ECP_C
-#define MBEDTLS_ECP_DP_SECP192R1_ENABLED
-#define MBEDTLS_ECP_DP_SECP224R1_ENABLED
-#define MBEDTLS_ECP_DP_SECP256R1_ENABLED
-#define MBEDTLS_ECP_DP_SECP384R1_ENABLED
-#define MBEDTLS_ECP_DP_SECP521R1_ENABLED
-#define MBEDTLS_ECP_DP_BP256R1_ENABLED
-#define MBEDTLS_ECP_DP_BP384R1_ENABLED
-#define MBEDTLS_ECP_DP_BP512R1_ENABLED
-#define MBEDTLS_ECP_DP_SECP192K1_ENABLED
-#define MBEDTLS_ECP_DP_SECP224K1_ENABLED
-#define MBEDTLS_ECP_DP_SECP256K1_ENABLED
-#define MBEDTLS_ECP_DP_CURVE25519_ENABLED
-#define MBEDTLS_ECP_DP_CURVE448_ENABLED
-
-#define MBEDTLS_PEM_PARSE_C
+/**
+ * \def MBEDTLS_PEM_WRITE_C
+ *
+ * Enable PEM encoding / writing.
+ *
+ * Module:  library/pem.c
+ * Caller:  library/pkwrite.c
+ *          library/x509write_crt.c
+ *          library/x509write_csr.c
+ *
+ * Requires: MBEDTLS_BASE64_C
+ *
+ * This modules adds support for encoding / writing PEM files.
+ */
 #define MBEDTLS_PEM_WRITE_C
-
-#define MBEDTLS_X509_USE_C
-#define MBEDTLS_X509_CRT_PARSE_C
-#define MBEDTLS_X509_CRL_PARSE_C
-#define MBEDTLS_X509_CSR_PARSE_C
-#define MBEDTLS_X509_CREATE_C
-#define MBEDTLS_X509_CRT_WRITE_C
-#define MBEDTLS_X509_CSR_WRITE_C
 
 #endif /* NATIVE_SIM_TLS_CONF_H */

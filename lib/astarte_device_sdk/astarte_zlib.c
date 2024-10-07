618e85b5f7e51235e067b20b4cc610b481347431
Simone Orru <simone.orru@secomind.com>
2024-10-07 16:39:28 +0200

    Use dedicated heap also in zlib
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- lib/astarte_device_sdk/astarte_zlib.c
+++ lib/astarte_device_sdk/astarte_zlib.c
@@ -5,6 +5,29 @@
  */
 #include "astarte_zlib.h"
 
+#include "heap.h"
+
+/************************************************
+ *         Static functions declaration         *
+ ***********************************************/
+
+/**
+ * @brief Custom allocation function for zlib.
+ *
+ * @param[in] opaque User data for the allocation function.
+ * @param[in] items Number of items to allocate.
+ * @param[in] size Size for each item to allocate.
+ * @return The allocated memory region, NULL upon failure.
+ */
+static voidpf zcalloc(voidpf opaque, uInt items, uInt size);
+/**
+ * @brief Custom deallocation function for zlib.
+ *
+ * @param[in] opaque User data for the allocation function.
+ * @param[in] address Address of the memory to deallocate.
+ */
+static void zcfree(voidpf opaque, voidpf address);
+
 /************************************************
  *         Global functions definitions         *
  ***********************************************/
@@ -21,8 +44,8 @@ int ZEXPORT astarte_zlib_compress(
     left = *destLen;
     *destLen = 0;
 
-    stream.zalloc = (alloc_func) 0;
-    stream.zfree = (free_func) 0;
+    stream.zalloc = (alloc_func) zcalloc;
+    stream.zfree = (free_func) zcfree;
     stream.opaque = (voidpf) 0;
 
     int windowBits = 9; // Smallest possible window
@@ -54,3 +77,78 @@ int ZEXPORT astarte_zlib_compress(
     return err == Z_STREAM_END ? Z_OK : err;
 }
 // NOLINTEND
+
+// NOLINTBEGIN: This function is pretty much identical to the one contained in zlib.
+int ZEXPORT astarte_zlib_uncompress(
+    Bytef *dest, uLongf *destLen, const Bytef *source, uLong sourceLen)
+{
+    z_stream stream;
+    int err;
+    const uInt max = (uInt) -1;
+    uLong len, left;
+    Byte buf[1]; /* for detection of incomplete stream when *destLen == 0 */
+
+    len = sourceLen;
+    if (*destLen) {
+        left = *destLen;
+        *destLen = 0;
+    } else {
+        left = 1;
+        dest = buf;
+    }
+
+    stream.next_in = (z_const Bytef *) source;
+    stream.avail_in = 0;
+    stream.zalloc = (alloc_func) zcalloc;
+    stream.zfree = (free_func) zcfree;
+    stream.opaque = (voidpf) 0;
+
+    err = inflateInit(&stream);
+    if (err != Z_OK)
+        return err;
+
+    stream.next_out = dest;
+    stream.avail_out = 0;
+
+    do {
+        if (stream.avail_out == 0) {
+            stream.avail_out = left > (uLong) max ? max : (uInt) left;
+            left -= stream.avail_out;
+        }
+        if (stream.avail_in == 0) {
+            stream.avail_in = len > (uLong) max ? max : (uInt) len;
+            len -= stream.avail_in;
+        }
+        err = inflate(&stream, Z_NO_FLUSH);
+    } while (err == Z_OK);
+
+    sourceLen -= len + stream.avail_in;
+    if (dest != buf)
+        *destLen = stream.total_out;
+    else if (stream.total_out && err == Z_BUF_ERROR)
+        left = 1;
+
+    inflateEnd(&stream);
+    return err == Z_STREAM_END                          ? Z_OK
+        : err == Z_NEED_DICT                            ? Z_DATA_ERROR
+        : err == Z_BUF_ERROR && left + stream.avail_out ? Z_DATA_ERROR
+                                                        : err;
+}
+// NOLINTEND
+
+/************************************************
+ *         Static functions definitions         *
+ ***********************************************/
+
+static voidpf zcalloc(voidpf opaque, unsigned items, unsigned size)
+{
+    (void) opaque;
+    return sizeof(uInt) > 2 ? (voidpf) astarte_malloc(items * size)
+                            : (voidpf) astarte_calloc(items, size);
+}
+
+static void zcfree(voidpf opaque, voidpf address)
+{
+    (void) opaque;
+    astarte_free(address);
+}
--- lib/astarte_device_sdk/include/astarte_zlib.h
+++ lib/astarte_device_sdk/include/astarte_zlib.h
@@ -21,12 +21,14 @@
 /**
  * @brief Function equivalent to the `compress` function defined in zlib.h.
  *
- * @details The implementation is copied 1 to 1 from zlib, with a single difference.
+ * @details The implementation is copied 1 to 1 from zlib, with some differences.
  * `defalteInit` has been replaced by `deflateInit2`. This allows to reduce the memory usage of zlib
  * by setting custom values for the `windowBits` and `memLevel` parameters.
  * This could also have been done by changing the values of the defines `MAX_WBITS` and
  * `DEF_MEM_LEVEL` in zconf.h. However this would have required to change the library sources and
  * has been avoided.
+ * Furthermore, the allocation and deallocation functions from the Astarte library have been used in
+ * place of the default ones.
  *
  * @note Since this function is almost a copy and paste from zlib.c, it will not be linted as every
  * other function in this library.
@@ -38,6 +40,27 @@
  * @return See docstrings for `compress` in zlib.h
  */
 int ZEXPORT astarte_zlib_compress(
+    Bytef *dest, uLongf *destLen, const Bytef *source, uLong sourceLen);
+// NOLINTEND
+
+// NOLINTBEGIN: This function is pretty much identical to the one contained in zlib.
+/**
+ * @brief Function equivalent to the `uncompress` function defined in zlib.h.
+ *
+ * @details The implementation is copied 1 to 1 from zlib, with a single difference.
+ * The allocation and deallocation functions from the Astarte library have been used in place of the
+ * default ones.
+ *
+ * @note Since this function is almost a copy and paste from zlib.c, it will not be linted as every
+ * other function in this library.
+ *
+ * @param dest See docstrings for `compress` in zlib.h
+ * @param destLen See docstrings for `compress` in zlib.h
+ * @param source See docstrings for `compress` in zlib.h
+ * @param sourceLen See docstrings for `compress` in zlib.h
+ * @return See docstrings for `compress` in zlib.h
+ */
+int ZEXPORT astarte_zlib_uncompress(
     Bytef *dest, uLongf *destLen, const Bytef *source, uLong sourceLen);
 // NOLINTEND
 

0b1b2b1aa9b2c15ca712eec33e7ef8d0eabaf3fb
Simone Orru <simone.orru@secomind.com>
2025-03-19 15:16:35 +0100

    Prepare release v0.8.0
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- include/astarte_device_sdk/astarte.h
+++ include/astarte_device_sdk/astarte.h
@@ -19,8 +19,8 @@
 /** @brief Major version number */
 #define ASTARTE_DEVICE_SDK_MAJOR 0
 /** @brief Minor version number */
-#define ASTARTE_DEVICE_SDK_MINOR 7
+#define ASTARTE_DEVICE_SDK_MINOR 8
 /** @brief Patch version number */
-#define ASTARTE_DEVICE_SDK_PATCH 2
+#define ASTARTE_DEVICE_SDK_PATCH 0
 
 #endif // ASTARTE_DEVICE_SDK_ASTARTE_H

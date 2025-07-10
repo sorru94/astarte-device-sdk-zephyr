bee3685975e37f936d8b4c341961282745279f55
Simone Orru <simone.orru@secomind.com>
2025-07-10 12:54:48 +0200

    WiFi sample
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

---
+++ samples/wifi_sample/src/wifi_test.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+#include "wifi.h"
+
+int main(void)
+{
+	printk("Started WiFi test application.\n");
+
+	app_wifi_init();
+
+	k_sleep(K_SECONDS(5));
+
+	printk("Attempting a WiFi connection.\n");
+	const char *ssid = CONFIG_WIFI_SSID;
+	enum wifi_security_type sec = WIFI_SECURITY_TYPE_PSK;
+	const char *psk = CONFIG_WIFI_PASSWORD;
+	app_wifi_connect(ssid, sec, psk);
+
+	while (true) {
+		k_sleep(K_SECONDS(5));
+	}
+
+	return 0;
+}

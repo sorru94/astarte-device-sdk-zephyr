1c66489481af067fa3fbcd5615e7a9f85fcfce89
Simone Orru <simone.orru@secomind.com>
2026-03-12 17:15:13 +0100

    End to end refactor: pytest orchestrator
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- e2e/pytest/http_requests.py
+++ e2e/pytest/http_requests.py
@@ -8,19 +8,20 @@ Contains useful wrappers for HTTPS requests.
 
 import base64
 import json
-from typing import Any
+import logging
 
 import curlify
 import requests
-from cfgvalues import CfgValues
 from dateutil import parser
+from datetime import datetime
 
-from west import log
-from datetime import datetime, timezone
+from configuration import Configuration
+
+logger = logging.getLogger(__name__)
 
 
-def get_server_data(
-    test_cfg: CfgValues,
+def http_get_server_data(
+    e2e_cfg: Configuration,
     interface: str,
     path: str | None = None,
     quiet: bool = False,
@@ -28,143 +29,124 @@ def get_server_data(
     since_after: datetime | None = None,
     to: datetime | None = None,
 ) -> object:
-    """
-    Wrapper for a GET request for the server returning the specified interface data.
-    """
     url = (
-        test_cfg.appengine_url
+        e2e_cfg.appengine_url
         + "/v1/"
-        + test_cfg.realm
+        + e2e_cfg.realm
         + "/devices/"
-        + test_cfg.device_id
+        + e2e_cfg.device_id
         + "/interfaces/"
         + interface
     )
 
-    if not path is None:
+    if path is not None:
         url += path
 
-    headers = {"Authorization": "Bearer " + test_cfg.appengine_token}
+    headers = {"Authorization": "Bearer " + e2e_cfg.appengine_token}
 
     params: dict[str, str] = {}
 
-    if not since_after is None:
+    if since_after is not None:
         params["since_after"] = since_after.isoformat()
 
-    if not limit is None:
+    if limit is not None:
         params["limit"] = str(limit)
 
-    if not to is None:
+    if to is not None:
         params["to"] = to.isoformat()
 
     res = requests.get(
-        url, headers=headers, params=params, timeout=5, verify=test_cfg.appengine_cert
+        url, headers=headers, params=params, timeout=5, verify=e2e_cfg.appengine_cert
     )
-    log.inf(curlify.to_curl(res.request))
+    logger.info(curlify.to_curl(res.request))
     if res.status_code != 200:
         if not quiet:
-            log.err(res.text)
+            logger.error(res.text)
         raise requests.HTTPError(f"GET request failed. response {res}")
 
     return res.json().get("data", {})
 
 
-def post_server_data(
-    test_cfg: CfgValues, interface: str, endpoint: str, data: dict, quiet: bool = False
+def http_post_server_data(
+    e2e_cfg: Configuration, interface: str, endpoint: str, data: dict, quiet: bool = False
 ):
-    """
-    Wrapper for a POST request for the server, uploading new values to an interface.
-    """
     url = (
-        test_cfg.appengine_url
+        e2e_cfg.appengine_url
         + "/v1/"
-        + test_cfg.realm
+        + e2e_cfg.realm
         + "/devices/"
-        + test_cfg.device_id
+        + e2e_cfg.device_id
         + "/interfaces/"
         + interface
         + endpoint
     )
     json_data = json.dumps({"data": data}, default=str)
     headers = {
-        "Authorization": "Bearer " + test_cfg.appengine_token,
+        "Authorization": "Bearer " + e2e_cfg.appengine_token,
         "Content-Type": "application/json",
     }
     res = requests.post(
-        url=url, data=json_data, headers=headers, timeout=5, verify=test_cfg.appengine_cert
+        url=url, data=json_data, headers=headers, timeout=5, verify=e2e_cfg.appengine_cert
     )
-    log.inf(curlify.to_curl(res.request))
+    logger.info(curlify.to_curl(res.request))
     if res.status_code != 200:
         if not quiet:
-            log.err(res.text)
+            logger.error(res.text)
         raise requests.HTTPError(f"POST request failed. response {res}")
 
 
-def unset_server_property(test_cfg: CfgValues, interface: str, endpoint: str, quiet: bool = False):
-    """
-    Wrapper for a DELETE request for the server, deleting an endpoint.
-    """
+def http_delete_server_data(
+    e2e_cfg: Configuration, interface: str, endpoint: str, quiet: bool = False
+):
     url = (
-        test_cfg.appengine_url
+        e2e_cfg.appengine_url
         + "/v1/"
-        + test_cfg.realm
+        + e2e_cfg.realm
         + "/devices/"
-        + test_cfg.device_id
+        + e2e_cfg.device_id
         + "/interfaces/"
         + interface
         + endpoint
     )
     headers = {
-        "Authorization": "Bearer " + test_cfg.appengine_token,
+        "Authorization": "Bearer " + e2e_cfg.appengine_token,
         "Content-Type": "application/json",
     }
-    res = requests.delete(url, headers=headers, timeout=5, verify=test_cfg.appengine_cert)
-    log.inf(curlify.to_curl(res.request))
+    res = requests.delete(url, headers=headers, timeout=5, verify=e2e_cfg.appengine_cert)
+    logger.info(curlify.to_curl(res.request))
     if res.status_code != 204:
         if not quiet:
-            log.err(res.text)
+            logger.error(res.text)
         raise requests.HTTPError("DELETE request failed.")
 
 
-# FIXME we should read the interface file to have access to the fields types instead of encoding based on field name
-def prepare_transmit_data(key, value) -> Any:
-    """
-    Some data to be transmitted should be encoded to an appropriate type.
-    """
-    if key == "binaryblob_endpoint":
+def http_prepare_transmit_data(interface, path, value):
+    logger.info(f"Preparing transmit data, interface {interface}, path {path}, value {value}")
+    mapping = interface.get_mapping(path)
+    if mapping.type == "binaryblob":
         return base64.b64encode(value).decode("utf-8")
-
-    if key == "binaryblobarray_endpoint":
+    if mapping.type == "binaryblobarray":
         return [base64.b64encode(v).decode("utf-8") for v in value]
     return value
 
 
-# FIXME we should read the interface file to have access to the fields types instead of decoding based on field name
-def decode_value(recv: Any, prop: str):
-    """
-    Decode a value read from astarte and map it approprietly to a python value
-    """
-    e: Any = recv
-
-    if (isinstance(prop, str) and prop.endswith("array_endpoint")) and (e == None):
+def http_decode_received_data(interface, path, received_data):
+    logger.info(
+        f"Decoding received data, interface {interface}, path {path}, value {received_data}"
+    )
+    mapping = interface.get_mapping(path)
+    if (received_data == None) and (mapping.type.endswith("array")):
         return []
-
-    # Parse longinteger from string to number (only necessary for aggregates)
-    if "longinteger_endpoint" == prop:
-        return int(e)
-    if "longintegerarray_endpoint" == prop:
-        return [int(dt) for dt in e]
-
-    # Parse datetime from string to datetime
-    if "datetime_endpoint" == prop:
-        return parser.parse(e)
-    if "datetimearray_endpoint" == prop:
-        return [parser.parse(dt) for dt in e]
-
-    # Decode binary blob from base64
-    if "binaryblob_endpoint" == prop:
-        return base64.b64decode(e)
-    if "binaryblobarray_endpoint" == prop:
-        return [base64.b64decode(dt) for dt in e]
-
-    return e
+    if mapping.type == "longinteger":
+        return int(received_data)
+    if mapping.type == "longintegerarray":
+        return [int(e) for e in received_data]
+    if mapping.type == "datetime":
+        return parser.parse(received_data)
+    if mapping.type == "datetimearray":
+        return [parser.parse(e) for e in received_data]
+    if mapping.type == "binaryblob":
+        return base64.b64decode(received_data)
+    if mapping.type == "binaryblobarray":
+        return [base64.b64decode(e) for e in received_data]
+    return received_data
